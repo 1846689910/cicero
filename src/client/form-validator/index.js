9435c919c2418a0afa8f36031cc5a7a341706620
1846689910 <eric14706@gmail.com>
2019-09-18 00:23:50 -0700

    mod

--- src/client/form-validator/index.js
+++ src/client/form-validator/index.js
@@ -1,12 +1,24 @@
+const defaultOptions4EVUnit = {
+  validStyle: {
+    border: "1px solid green",
+    boxShadow: "0 0 3px green"
+  },
+  invalidStyle: {
+    border: "1px solid red",
+    boxShadow: "0 0 3px red"
+  }
+};
 export class EVUnit {
   /**
    * @description form element validator unit used to wrap element and its validator function
    * @param {object}    element: a form element
    * @param {function}  validator: a validate function that will take the `element` as parameter, and return `boolean` value
+   * @param {object}    options: an object contains `validStyle`, `invalidStyle` for the element's validity itself
    */
-  constructor(element, validator) {
+  constructor(element, validator, options = defaultOptions4EVUnit) {
     this.element = element;
     this.validator = validator;
+    this.options = options;
   }
 }
 const defaultOptions = {
@@ -55,15 +67,24 @@ export class FormValidator {
   };
   _init = () => {
     this.evUnits.forEach((x, i) => {
+      this._setSingleElementStyle(x, i);
       const listener = () => {
         this._results[i] = x.validator(x.element);
+        this._setSingleElementStyle(x, i);
         this._triggerCallback();
       };
       x.element.addEventListener("input", listener, false);
       this._listeners.set(x.element, listener);
     });
   };
+
+  _setSingleElementStyle = (evUnit, i) =>
+    Object.entries(
+      this._results[i] ? evUnit.options.validStyle : evUnit.options.invalidStyle
+    ).forEach(([k, v]) => (evUnit.element.style[k] = v));
+
   isValid = () => this.options.validate(this._results);
+
   _triggerCallback = () => {
     this.isValid()
       ? this.options.validCallback()
